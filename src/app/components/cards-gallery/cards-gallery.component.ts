ed3f49094c3901ce7dfd461089eacb38ca1dd1e4
Mazin Fouad <m.fouad@live.de>
2023-05-10 12:05:11 +0200

    load more pokemons function added

--- src/app/components/cards-gallery/cards-gallery.component.ts
+++ src/app/components/cards-gallery/cards-gallery.component.ts
@@ -9,18 +9,74 @@ import { PokemonApiService } from 'src/services/pokemon-api.service';
 })
 export class CardsGalleryComponent implements OnInit {
   pokemons: any[] = [];
+  count: number = 12;
+
   constructor(private pokemonApiService: PokemonApiService) {}
 
   ngOnInit() {
-    this.fetchPokemons();
+    this.fetchPokemons(1, 12);
     console.log(this.pokemons);
   }
 
-  fetchPokemons() {
-    for (let i = 1; i <= 12; i++) {
+  fetchPokemons(startIndex: number, count: number) {
+    for (let i = startIndex; i < startIndex + count; i++) {
       this.pokemonApiService.getPokemons(i).subscribe((pokemon) => {
         this.pokemons.push(pokemon);
       });
+    }
+    this.count += count;
+  }
+
+  loadMorePokemons() {
+    let lastPokemonIndex = this.pokemons.length;
+    this.fetchPokemons(lastPokemonIndex, 4);
+    this.count += 4;
+  }
+
+  trackById(index: number, pokemon: any): number {
+    return pokemon.id;
+  }
+
+  getPokemonType(pokemon: any) {
+    switch (pokemon.types[0].type.name) {
+      case 'normal':
+        return 'normal';
+      case 'fire':
+        return 'fire';
+      case 'water':
+        return 'water';
+      case 'electric':
+        return 'electric';
+      case 'grass':
+        return 'grass';
+      case 'ice':
+        return 'ice';
+      case 'fighting':
+        return 'fighting';
+      case 'poison':
+        return 'poison';
+      case 'ground':
+        return 'ground';
+      case 'flying':
+        return 'flying';
+      case 'psychic':
+        return 'psychic';
+      case 'bug':
+        return 'bug';
+      case 'rock':
+        return 'rock';
+      case 'ghost':
+        return 'ghost';
+      case 'dragon':
+        return 'dragon';
+      case 'dark':
+        return 'dark';
+      case 'steel':
+        return 'steel';
+      case 'fairy':
+        return 'fairy';
+      default:
+        return '';
     }
   }
 }
